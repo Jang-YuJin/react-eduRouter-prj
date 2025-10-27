55de7516f2dda5b4c30abe10480785b9b6e23ef2
Jang-YuJin <58157591+Jang-YuJin@users.noreply.github.com>
2025-10-27 20:08:13 +0900

    Initialize products state as an empty array

--- src/page/AllProduct.jsx
+++ src/page/AllProduct.jsx
@@ -2,12 +2,12 @@ import React, { useEffect, useState } from 'react'
 import CardProduct from '../component/CardProduct'
 
 const AllProduct = () => {
-  const [products, setProducts] = useState(null);
+  const [products, setProducts] = useState([]);
 
   const getProducts = async() => {
     let response = await fetch('https://my-json-server.typicode.com/Jang-YuJin/react-eduRouter-prj/products');
     let data = await response.json();
-console.log(data);
+
     setProducts(data);
   };
 
