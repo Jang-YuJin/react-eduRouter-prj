3d4717b75f8156c0128b5215920e39d4d2d4dc9a
Jang-YuJin <58157591+Jang-YuJin@users.noreply.github.com>
2025-10-27 20:09:50 +0900

    Fix typo in CardProduct component

--- src/component/CardProduct.jsx
+++ src/component/CardProduct.jsx
@@ -6,7 +6,7 @@ const CardProduct = ({ item }) => {
       <div><img src={item?.img} alt={item?.title} className='card-img'></img></div>
       <div className='hot-container'>{item?.choice}</div>
       <div className='title-container'>{item?.title}</div>
-      <div className='new-container'>{iten?.new}</div>
+      <div className='new-container'>{item?.new}</div>
     </div>
   )
 }
