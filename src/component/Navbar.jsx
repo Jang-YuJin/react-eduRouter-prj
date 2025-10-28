756a4b4fb1d70b3695b2977c7d319c06ec110a96
Jang-YuJin <jangyj0912@naver.com>
2025-10-28 19:33:21 +0900

    메뉴, 전체상품 모바일 반응형

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { library } from '@fortawesome/fontawesome-svg-core'
 /* import all the icons in Free Solid, Free Regular, and Brands styles */
@@ -9,6 +9,8 @@ import { useNavigate } from 'react-router-dom'
 library.add(fas, far, fab)
 
 const Navbar = ({ setSelectMenu, selectMenu }) => {
+  const [onOff, setOnOff] = useState('off');
+  const [closeOnOff, setCloseOnOff] = useState('off');
   const navigate = useNavigate();
 
   const menuList = ['웹개발', 'JAVA', 'Python', 'DB', 'HTML/CSS', 'about'];
@@ -20,6 +22,16 @@ const Navbar = ({ setSelectMenu, selectMenu }) => {
   const goToMain = () => {
     setSelectMenu('');
     navigate('/');
+  }
+
+  const settingCloseBtnOn = () => {
+    setOnOff('on');
+    setCloseOnOff('on');
+  }
+
+  const settingCloseBtnOff = () => {
+    setOnOff('off');
+    setCloseOnOff('off');
   }
 
   useEffect(() => {console.log(selectMenu)}, [selectMenu]);
@@ -34,7 +46,8 @@ const Navbar = ({ setSelectMenu, selectMenu }) => {
         </div>
       </div>
       <div className='menu-container'>
-        <ul className='menu'>
+        <FontAwesomeIcon icon="fa-solid fa-bars" className='menu-bar' onClick={settingCloseBtnOn}/>
+        <ul className={`menu ${onOff}`}>
           {menuList.map((item, index) => (
             <li className={`menu-item ${selectMenu.item === item ? 'select-menu' : ''}`} id={index} onClick={() => setSelectMenu({item})}>{item}</li>
           ))}
@@ -42,6 +55,9 @@ const Navbar = ({ setSelectMenu, selectMenu }) => {
         <div className='search-container'>
           <FontAwesomeIcon icon="fa-solid fa-magnifying-glass" className='search-btn'/>
           <input type='text' placeholder='강의를 검색해보세요.'></input>
+        </div>
+        <div className={`close-container ${closeOnOff}`}>
+          <button className='menu-close-btn' onClick={settingCloseBtnOff}>X</button>
         </div>
       </div>
     </div>
